131a247ed05631e7a1b20eb73128e66bad97f470
ArcFarM <hinsung99@gmail.com>
2025-05-20 17:14:29 +0900

    0520-2

--- Assets/Scripts/Interaction/GetWeapon.cs
+++ Assets/Scripts/Interaction/GetWeapon.cs
@@ -1,20 +1,27 @@
-using TMPro;
 using UnityEngine;
+using TMPro;
+using Palmmedia.ReportGenerator.Core.CodeAnalysis;
 using UnityEngine.InputSystem;
 
 namespace FPSSample {
-    public class DoorOpen : MonoBehaviour {
-        //문 열기 상호작용 대상에 부착
+    public class GetWeapon : MonoBehaviour
+    {
         #region Variables
+        //대상과 플레이어 사이의 거리
         float distance;
         [SerializeField] float threshold = 2f;
-        //문 열기 메시지를 띄울 UI
+        //상호작용 시 나올 대상
         [SerializeField] GameObject actionUI;
         [SerializeField] TextMeshProUGUI actionText;
-        //문 열고 닫는 애니메이션
-        [SerializeField] Animator doorAnimator;
+        Animator weaponAnimator;
+        string actionString = "총 집기";
+        [SerializeField] GameObject playerWeapon;
+        //강조 화살표
+        [SerializeField] GameObject highlightArrow;
         //상호작용 가능한 대상 조우시 십자선 활성화
         [SerializeField] GameObject crosshair;
+        //실행되면 한 번만 실행되어야 함
+        bool onceFlag = false;
         #endregion
 
         #region Unity Event Methods
@@ -22,18 +29,17 @@ namespace FPSSample {
             distance = CheckDistance.distance;
         }
         private void OnMouseOver() {
-            if(distance <= threshold) {
+            if (distance <= threshold && !onceFlag) {
                 ShowActionUI();
             }
             else {
-                 HideActionUI();
+                HideActionUI();
             }
         }
         private void OnMouseExit() {
             HideActionUI();
         }
         #endregion
-
         #region Custom Methods
         void ShowActionUI() {
             actionUI.SetActive(true);
@@ -44,14 +50,16 @@ namespace FPSSample {
             crosshair.SetActive(false);
         }
         public void OnInteract(InputAction.CallbackContext context) {
-            //문 열라고 문구 나오게 하기
-            actionText.text = "문 열기";
-            //TODO : 열린 문에 다시 상호작용하면 문닫기
-            if (context.started) {
-                if (distance <= threshold) {
-                    //문 열기
-                    doorAnimator.SetBool("IsOpen", true);
-                    GetComponent<BoxCollider>().enabled = false;
+            //무기 획득
+            if (distance <= threshold) {
+                if (!onceFlag) {
+                    onceFlag = true;
+                    actionText.text = actionString;
+                    //TODO : 무기 장착 애니메이션 재생
+                    playerWeapon.SetActive(true);
+                    //다 끝났다면 콜라이더를 꺼서 상호작용 없애기
+                    highlightArrow.SetActive(false);
+                    this.gameObject.SetActive(false);
                 }
             }
         }
